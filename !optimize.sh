e0f8211d8f97518aad30c749cc662a6b308da47e
yuuukun <58155530+kamishirorui@users.noreply.github.com>
2022-09-10 09:43:23 +0700

    updates

--- !optimize.sh
+++ !optimize.sh
@@ -1,6 +1,8 @@
 
 # drive -> local
 rclone copy "RaisDrive:/Enstars2/Shared/frameless cards" assets
+rclone copy "RaisDrive:/Enstars2/Shared/cgs" assets
+rclone copy "RaisDrive:/Enstars2/Shared/renders" assets
 # WARNING: Don't use rclone sync as it may delete files already on there
 
 # convert image
@@ -10,5 +12,14 @@ bash png2webp.sh assets 90
 # optimize images
 bash optimize-jpg.sh assets 95
 
+# combined
+bash png2jpg.sh assets 90 && bash png2webp.sh assets 90 && bash optimize-jpg.sh assets 95
+
 # local -> backblaze
 rclone copy assets "Backblaze:ensemble-square/assets"
+
+# others
+
+rclone copy "Backblaze:ensemble-square/render" render
+bash png2jpg.sh render 90 && bash png2webp.sh render 90 && bash optimize-jpg.sh render 95
+rclone copy render "Backblaze:ensemble-square/render" 
